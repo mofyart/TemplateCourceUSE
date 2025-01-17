1b8ba21a8e6e8ee04186bacc4710e92bc5795031
Mofyart <busygin.artem05mail.ru>
2025-01-18 00:34:25 +0300

    CourseMathUSE

---
+++ menu.js
@@ -0,0 +1,8 @@
+﻿$(document).ready(function() {
+	$('.menu-burger__header').click(function(){
+        $('.menu-burger__header').toggleClass('open-menu');
+        $('.header__nav').toggleClass('open-menu');
+        $('body').toggleClass('fixed-page');
+	});
+});
+
